dc39384053c2bf7ae6de91ed39f98959e98341c6
StormHive <saadrashid334@gmail.com>
2024-04-24 18:40:46 +0500

    added run.sh file

---
+++ indeed_scrapper/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Find the directory and change to it
+cd "$(find ~ -type d -name "indeed_scrapper" -path "*Indeed-Scrapper*" 2>/dev/null | head -n 1)"
+
+if [ $? -eq 0 ]; then
+    echo "Found 'indeed-scraper' directory and changed into it."
+    
+    # Activate the virtual environment
+    source .venv/bin/activate
+    
+    if [ $? -eq 0 ]; then
+        echo "Activated virtual environment."
+        
+        # Install requirements
+        pip install -r requirements.txt
+        
+        if [ $? -eq 0 ]; then
+            echo "Installed requirements."
+            
+            # Check if pm2 is required
+            if ! command -v pm2 &> /dev/null; then
+                echo "pm2 is not installed. Installing..."
+                
+                # Check if npm is installed
+                if ! command -v npm &> /dev/null; then
+                    echo "npm is not installed. Installing..."
+                    
+                    # Check if node is installed
+                    if ! command -v node &> /dev/null; then
+                        echo "node is not installed. Installing..."
+                        brew install node
+                    fi
+                    
+                    # Install npm
+                    brew install npm
+                fi
+                
+                # Install pm2
+                npm install pm2@latest -g
+            fi
+            
+            # Start the Django project with pm2
+            pm2 start echosystem.config.json
+        else
+            echo "Failed to install requirements."
+        fi
+    else
+        echo "Failed to activate virtual environment."
+    fi
+else
+    echo "Could not find 'indeed-scraper' directory."
+fi
+
